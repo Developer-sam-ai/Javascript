43258931a0c55613a1049f84e3761587abbef1d4
sam <sam.developer31@gmail.com>
2025-06-24 23:32:55 +0530

    arrow and IIFE function

--- Function and more/02_ScopeofFunc.js
+++ Function and more/02_ScopeofFunc.js
@@ -6,8 +6,43 @@ var j=67;
 // console.log(j);
 {}  //scope 
 // and object {}are the object 
-if(true){
-    z=300;
-}
-console.log(z);
+// if(true){
+//     z=300;
+// }
+// console.log(z);
 
+// function one(){
+//     let a=34;
+//     two();
+//     function two(){
+//         let b=56;
+//         console.log(a);
+//     } 
+//     console.log(b);
+    
+// }
+// understand it like as we can get icecream from our big brother but he cannot
+// similarly two can access a variable but cannot access b
+// code is executed line by line
+
+// one();
+
+// if(true){
+//     let name="samudi";
+//     if(name==="samudi"){
+//         let name2=" bimidi"
+//         console.log(name+name2);
+//     }
+    //console.log(name2);
+    // }
+
+let storage=function(a,b){
+    console.log(a+b);
+    //  a+b;
+}
+console.log(storage(2,4));
+// But the function doesn't return anything ➝ so its return value is undefined
+// output  6
+//     undefined
+
+//Arrow function 
---
+++ Function and more/03_Arrow.js
@@ -0,0 +1,54 @@
+// let user={
+//     name:"sam",
+//     password:456,
+//     welcomMessege:function(){
+//         `welcome ${this.name} `;
+//         console.log(this);
+        
+//     }
+// }
+// console.log(user.welcomMessege());
+// let aray1=[23,45,`display flex`];
+
+// user.name=`radha`;
+// console.log(user.welcomMessege());
+// console.log(this);  // refering empty {}
+
+// function This(){
+//     console.log(this);
+    
+//  }
+// This();
+//  hell kind of things in it inside node environment
+// we cant use this keyword inside any function used for only object
+
+// Arrow function
+
+// let arrw=(num1,num2)=>{
+//     console.log(num1*num2);
+    
+//     console.log(`hello sam`);
+//     console.log(this);
+    
+// }
+// arrw(5,2);
+
+
+// let aroe=(num1,num2)=> num1+num2;
+// let aro=(num1,num2)=> (num1-num2);
+// using parenthesis will return values automatically
+// console.log(aroe(2,4));
+// console.log(aro(78,2));
+
+
+// Immediately invoked function expression
+// used when we want to avoid or bypass global scope pollution
+(function connect(){
+    console.log(`database connected`);
+    
+})();
+// semicolon do not forgot
+((name)=>{
+    console.log(`hello sam ${name}`);
+    
+})(`hello`)
