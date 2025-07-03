179860013719ff788cad3fa8f75e836ea312844d
sam <sam.developer31@gmail.com>
2025-07-03 21:51:43 +0530

    project JS
    started new project it is the first one to perform and did a decent job revise event listeners

---
+++ JS project(Basic)/color changer/color.js
@@ -0,0 +1,26 @@
+const change=document.querySelectorAll(".button");
+const body=document.querySelector("body");
+
+    change.forEach(function(button){
+        // console.log(lang);
+        button.addEventListener('dblclick',function(e){
+            console.log(e)
+            console.log(e.target);
+                    switch(e.target.id){
+                        case 'grey':
+                    body.style.backgroundColor='grey';
+                    break;
+                        case 'blue':
+                    body.style.backgroundColor='blue';
+                    break;
+                        case 'red':
+                    body.style.backgroundColor='red';
+                    break;
+                        case 'yellow':
+                    body.style.backgroundColor='yellow';
+                        break;
+                    }
+        });
+        });
+
+
