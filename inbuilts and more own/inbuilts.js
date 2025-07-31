488405a1a02ff434484af2d58e3bf73019845db8
sam <sam.developer31@gmail.com>
2025-07-31 23:10:44 +0530

    general documentation read

---
+++ inbuilts and more own/inbuilts.js
@@ -0,0 +1,36 @@
+// let hello=[2,3,4,5,6];
+//reduce and more
+
+// const sum=hello.reduce((a,c)=>
+//     a+c,0,
+// );
+// console.log(sum);
+
+
+// as here we did with zero
+// The first time that the callback is run there is no "return value of the previous calculation". If supplied, an initial value may be used in its place. Otherwise the array element at index 0 is used as the initial value and iteration starts from the next element (index 1 instead of index 0).
+// here we came through .reduce function where 
+
+
+
+// let arr=[1,2,3,4,5];
+
+// console.log(arr.map((e)=>
+//     Math.pow(e,2)
+// ))
+// [ 1, 4, 9, 16, 25 ]
+// output
+// it is the output because we putted the curly bracket in it so do not put curly brackets
+//  undefined, undefined, undefined, undefined, undefined, undefined ]
+
+
+// filter ()
+
+let arr1=[1,2,3,4,5,65];
+
+// console.log(arr1.filter((e) => e> 3));
+
+// by using string we can just take out the length and then print the component more than 6 word and more things 
+
+console.log(typeof(arr1))
+
